6ec6b9f862e78f2fabfb0d52e05e05913f915eeb
Marinac <nikola@regex-sh.com>
2021-08-16 23:15:37 +0200

    Create header component

---
+++ frontend-developer-tests/countries/src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const Header: React.FC = () => {
+  return (
+    <header className="header">
+      <h1 className="title">Countries</h1>
+      <p className="note">Click on the country to expand</p>
+    </header>
+  );
+};
