696af6e69fa783754cd4606adc319e5845f800ca
Marinac <nikola@regex-sh.com>
2021-08-16 23:15:53 +0200

    Update app file with countries component and logic

--- frontend-developer-tests/countries/src/App.tsx
+++ frontend-developer-tests/countries/src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import "./App.css";
+import { Countries } from "./components/Countries";
+import { Header } from "./components/Header";
+import Container from "@material-ui/core/Container";
+
+export const App: React.FC = () => {
+  const [countriesWithUsers, setCountriesWithUsers] = useState<Array<any>>([]);
+
+  const fetchData = async () => {
+    return await fetch("https://randomuser.me/api/?results=100")
+      .then((response) => response.json())
+      .then(({ results: users }) => {
+        const data = users.reduce((acc: any, user: any) => {
+          const location = user.location.country;
+          acc[location] = acc[location] || [];
+          acc[location].push(user);
+          return acc;
+        }, {});
+
+        const keys: Array<string> = Object.keys(data);
+        const vals: Array<object> = Object.values(data);
+
+        let countriesWithUsers = [];
+
+        for (let i = 0; i < keys.length; i++) {
+          let countryData = [keys[i], vals[i]];
+          countriesWithUsers.push(countryData);
+        }
+
+        countriesWithUsers.sort((a: Array<any>, b: Array<any>) => b[1].length - a[1].length);
+
+        setCountriesWithUsers(countriesWithUsers);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <Header />
+      <Countries countriesWithUsers={countriesWithUsers} />
+    </Container>
   );
-}
-
-export default App;
+};
